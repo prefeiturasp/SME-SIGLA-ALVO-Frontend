b518f12c7d4a0620270fd1019270434128e386ac
Vitor Campos <vitor.campos@spassu.com.br>
2025-10-01 14:29:22 -0300

    chore: Refatoração do uso de envs ao criar o axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,25 +1,17 @@
 import axios from "axios";
 
-const getEnv = (key: string, fallback: string) => {
-  // No Vite, as variáveis de ambiente são acessadas através de import.meta.env
-  // e devem ter o prefixo VITE_
-  const viteKey = `VITE_${key}`;
-  const envValue = import.meta.env[viteKey];
-  return envValue || fallback;
-};
-
 export const appAxiosProcessoConvocacao = axios.create({
-  baseURL: getEnv("PROCESSOS_CONVOCACAO_API_URL", "http://127.0.0.1:8000"),
+  baseURL: import.meta.env.VITE_PROCESSOS_CONVOCACAO_API_URL,
 });
 
 export const appAxiosConcursos = axios.create({
-  baseURL: getEnv("CONCURSOS_API_URL", "http://localhost:8001"),
+  baseURL: import.meta.env.VITE_CONCURSOS_API_URL,
 });
 
 export const appAxiosCandidatos = axios.create({
-  baseURL: getEnv("CANDIDATOS_API_URL", "http://localhost:8002"),
+  baseURL: import.meta.env.VITE_CANDIDATOS_API_URL,
 });
 
 export const appAxiosImportaArquivos = axios.create({
-  baseURL: getEnv("IMPORTACAO_ARQUIVOS_API_URL", "http://localhost:8003"),
+  baseURL: import.meta.env.VITE_IMPORTACAO_ARQUIVOS_API_URL,
 });
