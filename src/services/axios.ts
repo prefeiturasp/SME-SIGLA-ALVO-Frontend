e3fc24c0149adbf34497c1195580931ecf408dc9
Vitor Campos <vitor.campos@spassu.com.br>
2025-10-01 14:55:09 -0300

    chore: Debug axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+console.log("import.meta.env.VITE_PROCESSOS_CONVOCACAO_API_URL", import.meta.env.VITE_PROCESSOS_CONVOCACAO_API_URL)
+console.log("import.meta.env.VITE_CONCURSOS_API_URL", import.meta.env.VITE_CONCURSOS_API_URL)
+console.log("import.meta.env.VITE_CANDIDATOS_API_URL", import.meta.env.VITE_CANDIDATOS_API_URL)
+console.log("import.meta.env.VITE_IMPORTACAO_ARQUIVOS_API_URL", import.meta.env.VITE_IMPORTACAO_ARQUIVOS_API_URL)
+
 export const appAxiosProcessoConvocacao = axios.create({
   baseURL: import.meta.env.VITE_PROCESSOS_CONVOCACAO_API_URL,
 });
